0858a43fb9d6015f1e6d3d13478cedcc2a442117
faizanmukhtar337 <148056789+faizanmukhtar337@users.noreply.github.com>
2025-08-29 11:59:01 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~w3l1xJID#dPorUWJLHnqU7jDNB_Z2lB73xk_v3YTcqhzEkIhalRw",  // add your Session Id here
   
   OWNER_NUMBER: "263714757857", // put your phone number here
   
